84fc65334a37ffe2d5b9fc4b50bfcf59580b49c8
naga charan <charandonga9010@gmail.com>
2025-11-03 20:43:39 +0530

    Add border radius and alt text to logo image

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,10 @@ function App() {
             {/* Logo */}
             <div className="flex items-center space-x-2">
               <div className="w-10 h-10  flex items-center justify-center">
-                <img src={hm_logo} />
+                <img src={hm_logo}
+                  style={{ borderRadius: "15%" }} 
+                  alt="logo"
+                  />
               </div>
               <span className="text-xl font-bold text-gray-800">Healthy Handi</span>
             </div>
